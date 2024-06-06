93b38e626ab46241891238f7a653611134eb940d
Priyanshu <mehtapriyanshu408@gmail.com>
2024-06-06 23:15:16 +0530

    progress in frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
     return (
         <div className="">
             <Navigation />
+            <div>
+                <HomePage/>
+            </div>
         </div>
     );
 }
