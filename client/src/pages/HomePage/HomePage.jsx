1831e4e1d655c8fe33dafc95a954007e3c69d1b1
Priyanshu <mehtapriyanshu408@gmail.com>
2024-06-11 22:30:43 +0530

    git UI updates

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -6,7 +6,12 @@ const HomePage = () => {
     return (
         <div>
             <HomeCarousel/>
-            <div>
+
+            <div className='py-20 space-y-10 flex flex-col justify-center px-5 lg:px-10'>
+                <ProductCarousel/>
+                <ProductCarousel/>
+                <ProductCarousel/>
+                <ProductCarousel/>
                 <ProductCarousel/>
             </div>
         </div>
