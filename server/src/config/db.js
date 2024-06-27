10efe66ef5c1c88969a0ac1b48e7ebb203efc113
Priyanshu <97157296+priyanshum143@users.noreply.github.com>
2024-06-27 11:03:20 +0530

    Update db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = "mongodb+srv://mehtapriyanshu408:v8SXzLpsON8hHq6b@cluster0.lopkwvp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const URI = <your_mongoDB_URI>;
 const connectDB = () => {
     return mongoose.connect(URI);
 };
